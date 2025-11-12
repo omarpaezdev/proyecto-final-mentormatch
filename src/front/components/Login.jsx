e546697c150bc381b9abb70ebbcd18cdf402368f
Omar Páez <135053433+omarpaezdev@users.noreply.github.com>
2025-11-12 22:46:47 +0000

    render

--- src/front/components/Login.jsx
+++ src/front/components/Login.jsx
@@ -59,7 +59,7 @@ const Login = () => {
         <div className="container justify-items-center w-50 my-5">
             <h1 className="text-center col-12">Login </h1>
             <div className="d-flex justify-content-center" >
-                <img src="src/front/assets/img/MM-2.png" alt="Hero Illustration" className="logo-image"></img>
+                <img src="https://res.cloudinary.com/dp6e1sg4y/image/upload/v1762979051/MM-2.png" alt="Hero Illustration" className="logo-image"></img>
             </div>
             <div className="d-flex justify-content-center">
                 <form className="form-style p-4" onSubmit={handleSubmit}>
